1427b41c41cb5d8f7a33fe7b5bfc13d1c44ef151
Mathias Nonohay <mathias12300@gmail.com>
2021-01-04 17:15:05 -0300

    testes unitarios e novos métodos indtroduzidos ao código

---
+++ LeilaoTests/AvaliadorTests.swift
@@ -0,0 +1,76 @@
+//
+//  AvaliadorTests.swift
+//  LeilaoTests
+//
+//  Created by Mathias Almeida Nonohay on 1/4/21.
+//  Copyright © 2021 Alura. All rights reserved.
+//
+
+import XCTest
+@testable import Leilao
+
+class AvaliadorTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testDeveEntenderLancesEmOrdemCrescente() {
+        // Cenario
+        
+        let joao = Usuario(nome: "Joao")
+        let jose = Usuario(nome: "Jose")
+        let maria = Usuario(nome: "Maria")
+        
+        let leilao = Leilao(descricao: "Playstation 4")
+        leilao.propoe(lance: Lance(maria, 250.0))
+        leilao.propoe(lance: Lance(joao, 300.0))
+        leilao.propoe(lance: Lance(jose, 400.0))
+        
+        // Acao
+        
+        let leiloeiro = Avaliador()
+        leiloeiro.avalia(leilao: leilao)
+        
+        // Validacao
+        XCTAssertEqual(250.0, leiloeiro.menorLance())
+        XCTAssertEqual(400.0, leiloeiro.maiorLance())
+    }
+    
+    func testDeveEntenderLeilaoComApenasUmLance() {
+        let joao = Usuario(nome: "Joao")
+        let leilao = Leilao(descricao: "Playstation 4")
+        leilao.propoe(lance: Lance(joao, 1000.0))
+        
+        let leiloeiro = Avaliador()
+        leiloeiro.avalia(leilao: leilao)
+        
+        XCTAssertEqual(1000.0, leiloeiro.menorLance())
+        XCTAssertEqual(1000.0, leiloeiro.maiorLance())
+    }
+    
+    func testDeveEncontrarOsTresMaioresLances() {
+        let joao = Usuario(nome: "joao")
+        let maria = Usuario(nome: "maria")
+        
+        let leilao = Leilao(descricao: "playstation 4")
+        leilao.propoe(lance: Lance(joao, 300.0))
+        leilao.propoe(lance: Lance(maria, 400.0))
+        leilao.propoe(lance: Lance(joao, 500.0))
+        leilao.propoe(lance: Lance(maria, 600.0))
+        
+        let leiloeiro = Avaliador()
+        leiloeiro.avalia(leilao: leilao)
+        
+        let listaLances = leiloeiro.tresMaiores()
+        
+        XCTAssertEqual(3, listaLances.count)
+        XCTAssertEqual(600.0, listaLances[0].valor)
+        XCTAssertEqual(500.0, listaLances[1].valor)
+        XCTAssertEqual(400.0, listaLances[2].valor)
+    }
+}
